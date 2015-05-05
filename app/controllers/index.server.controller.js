5170c1767379ceb06ef937f37bea73d76484b45e
DangerRanger <dangerousdevops@gmail.com>
2015-05-05 13:20:14 -0700

    support logo

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,5 +1,5 @@
 exports.render = function(req, res) {
   res.render('index', {
     title: "Choons Rebuild"
-  })
+  })  
 };
--- config/express.js
+++ config/express.js
@@ -19,6 +19,8 @@ module.exports = function() {
   app.set('views', './app/views');
   app.set('view engine', 'ejs');
 
+  app.use(express.static('./public'));
+  
   require('../app/routes/index.server.routes.js')(app);
 
 return app;
