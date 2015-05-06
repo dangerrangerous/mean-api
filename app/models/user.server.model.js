210c7aee6262fdf1e38a6d5f119597b61b4ca79c
DangerRanger <dangerousdevops@gmail.com>
2015-05-06 11:30:48 -0700

    user link url

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -5,8 +5,30 @@ var UserSchema = new Schema({
   firstName: String,
   lastName: String,
   email: String,
-  username: String,
-  password: String
+  username: {
+    type: String,
+    trim: true
+  },
+  website: {
+    type: String,
+    set: function(url) {
+      if (!url) {
+        return url;
+      } else {
+        if (url.indexOf('http://') !== 0 && url.indexOf('https://')
+ !== 0) {
+          url = 'https://' + url;
+        }
+        return url;
+        }
+    }
+  },
+  password: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
+UserSchema.set('toJSON', { getters: true });
 mongoose.model('User', UserSchema);
