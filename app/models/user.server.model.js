7de0d7d9e5e0f1c93f3971f2e9e667b1644ec761
DangerRanger <dangerousdevops@gmail.com>
2015-05-06 13:58:27 -0700

    user schema auth

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose'),
+  crypt = require('crypto'),
   Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -55,10 +56,51 @@ UserSchema.virtual('fullname').get(function() {
   this.firstName = fullName[0] || '';
   this.lastName = fullName[1] || '';
 });
+
+UserSchema.pre('save', function(next) {
+  if (this.password) {
+    this.salt = new
+      Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+    this.password = this.hashPassword(this.password);
+  }
+  next();
+});
+
+UserSchema.methods.hashPassword = function(password) {
+  return crypto.pbkdf2Sync(password, this.salt, 10000,
+    64).toString('base64');
+};
+
+UserSchema.methods.authenticate = function(password) {
+  return this.password === this.hashPassword(password);
+};
+
 // static method
 UserSchema.statics.findOneByUsername = function(username, callback) {
   this.findOne({ username: new RegExp (username, 'i') }, callback);
 };
+
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+  var _this = this;
+  var possibleUsername = username + (suffix || '');
+
+  _this.findOne({
+    username: possibleUsername
+  }, function(err, user) {
+    if (!err) {
+      if (!user) {
+        callback(possibleUsername);
+      } else {
+        return _this.findUniqueUsername(username, (suffix || 0) +
+          1, callback);
+        }
+      } else {
+        callback(null);
+      }
+    });
+};
+
+
 // mongoose modifier
 UserSchema.set('toJSON', { getters: true, virtuals: true });
 
