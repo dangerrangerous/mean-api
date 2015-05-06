5df83fb3e1330245a553cb4486fa836e1c3e72ff
DangerRanger <dangerousdevops@gmail.com>
2015-05-06 11:05:34 -0700

    shingo nakamura

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -38,3 +38,13 @@ exports.userByID = function(req, res, next, id) {
     }
   });
 };
+
+exports.update = function(req, res, next) {
+  User.findByIdAndUpdate(req.user.id, req.body, function(err, user) {
+    if(err) {
+      return next(err);
+    } else {
+      res.json(user);
+    }
+  });
+};
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -4,4 +4,10 @@ module.exports = function(app) {
   app.route('/users')
     .post(users.create)
     .get(users.list);
+
+  app.route('/users/:userId')
+    .get(users.read)
+    .put(users.update);
+
+  app.param('userId', users.userByID);
 };
