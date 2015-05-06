fdda73cddb38fda1f6508bdc617d943ae3b02de5
DangerRanger <dangerousdevops@gmail.com>
2015-05-06 14:16:25 -0700

    u.c. signup and signout

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -44,6 +44,35 @@ exports.renderSignup = function(req, res, next) {
   }
 };
 
+exports.signup = function(req, res, next) {
+  if (!req.user) {
+    var user = new User(req.body);
+    var message = null;
+
+    user.provider = 'local';
+
+    user.save(function(err) {
+      if (err) {
+        var message = getErrorMessage(err);
+
+        req.flash('error', message);
+        return res.redirect('/signup');
+      }
+      req.login(user, function(err) {
+        if (err) return next(err);
+        return res.redirect('/');
+      });
+    });
+  } else {
+    return res.redirect('/');
+  }
+};
+
+exports.signout = function(req, res) {
+  req.logout();
+  res.redirect('/');
+};
+
 exports.create = function(req, res, next) {
   var user = new User(req.body);
 
