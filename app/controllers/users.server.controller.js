8db5c82b021d0f3f487242dbaa3de94f3cf3aaa2
DangerRanger <dangerousdevops@gmail.com>
2015-05-06 14:09:51 -0700

    user controller getErrorMessage

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,4 +1,27 @@
-var User = require('mongoose').model('User');
+var User = require('mongoose').model('User'),
+  passport = require('passport');
+
+var getErrorMessage = function(err) {
+  var message = '';
+
+  if (err.code) {
+    switch (err.code) {
+      case 11000:
+        case 11001:
+          message = 'Username already exists';
+          break;
+        default:
+          message = 'Something went wrong';
+        }
+      } else {
+        for (var errName in err.errors) {
+          if (err.errors[errName].message) message = err.errors[errName].
+    message;
+        }
+      }
+      return message;
+    };
+
 
 exports.create = function(req, res, next) {
   var user = new User(req.body);
